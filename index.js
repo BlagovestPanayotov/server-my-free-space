e6ca15de7fe019a2cf1364d99653ea461eb362a5
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-08-31 15:00:10 +0100

    connect to db

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
 
-const app = express();
+const cors = require('./middlewares/cors');
 
-app.listen(3000);
+const DB_URL = process.env.DATABASE_URL
+
+
+start();
+
+async function start() {
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/my-free-space');
+
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.get('/destianations', (req, res) => {
+      res.json({ message: 'hello' });
+    });
+
+    app.listen(3030, () => console.log('>>> Server listening on port 3030...'));
+}
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  hashedPassword: { type: String, required: true },
+  country: { type: String, required: true },
+  gender: { type: String, required: true }
+});
+
+userSchema.index({ email: 1 }, {
+  collation: {
+    locale: 'en',
+    strength: 2
+  }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
