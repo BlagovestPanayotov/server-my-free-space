3a053b322d0e6f78d1a5bab492967430e28480de
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-08-31 16:37:28 +0100

    update token parser

--- utils/tokenParser.js
+++ utils/tokenParser.js
@@ -1,17 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 
-function createToken(user) {
+function createPayload(user) {
   const payload = {
     _id: user._id,
     username: user.email,
     gender: user.gender
   };
 
-  return jwt.sign(payload, process.env.JWT_SECRET);
+  return {
+    _id: user._id,
+    username: user.username,
+    gender: user.gender,
+    accessToken: jwt.sign(payload, process.env.JWT_SECRET)
+  };
 
 };
 
 module.exports = {
-  createToken
+  createPayload
 };
