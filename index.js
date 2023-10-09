7a36b9f90b22ba7411f0480a8bbfd5f59f497a66
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-10-09 14:53:50 +0100

    add try-catch in index.js

--- index.js
+++ index.js
@@ -10,29 +10,33 @@ const session = require('./middlewares/session');
 const authController = require('./controllers/authController');
 const destinationController = require('./controllers/destinationController');
 const accessoryDestController = require('./controllers/accessorydestController');
+const User = require('./models/User');
 
 
 start();
 
 async function start() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
-  await mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+    const app = express();
 
-  const app = express();
+    app.use(express.json({ limit: '50mb' }));
+    app.use(cors(corsConfig));
+    app.use(trimBody());
+    app.use(session());
 
+    app.use('/users', authController);
+    app.use('/dest', destinationController);
+    app.use('/accessory', accessoryDestController);
 
- 
-  app.use(express.json({limit:'50mb'}));
-  app.use(cors(corsConfig));
-  app.use(trimBody());
-  app.use(session());
+    app.listen(3030, () => console.log('>>> Server listening on port 3030...'));
 
-  app.use('/users', authController);
-  app.use('/dest', destinationController);
-  app.use('/accessory', accessoryDestController);
-
-  app.listen(3030, () => console.log('>>> Server listening on port 3030...'));
+  } catch (error) {
+    console.error('Error:', error);
+    mongoose.disconnect();
+  }
 }
