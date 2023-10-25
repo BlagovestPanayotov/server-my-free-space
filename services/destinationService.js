e117f81e9d7ae653af43f2f522046537a95d72b9
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-10-25 23:40:48 +0100

    improve destination service

--- services/destinationService.js
+++ services/destinationService.js
@@ -2,154 +2,86 @@ const { Types } = require('mongoose');
 
 const Destination = require("../models/Destination");
 
+const pipelineDestList = () => [
+  {
+    $lookup: {
+      from: 'likedestinations',
+      localField: '_id',
+      foreignField: '_destinationId',
+      as: 'likes',
+    },
+  },
+  {
+    $lookup: {
+      from: 'comments',
+      localField: '_id',
+      foreignField: '_destinationId',
+      as: 'comments',
+    },
+  },
+  {
+    $lookup: {
+      from: 'users',
+      localField: '_ownerId',
+      foreignField: '_id',
+      as: 'owner',
+    },
+  },
+  {
+    $unwind: '$owner',
+  },
+  {
+    $project: {
+      name: 1,
+      description: 1,
+      img: '$img.thumbUrl',
+      _ownerId: 1,
+      likeCount: { $size: '$likes' },
+      commentCount: { $size: '$comments' },
+      ownerInfo: {
+        thumbUrl: '$owner.image.thumbUrl',
+        gender: '$owner.gender',
+        accountName: '$owner.accountName',
+      },
+    },
+  },
+];
+
 async function getAll(name, country, offset, pageSize) {
   const nameRegexp = new RegExp(name, 'i');
 
-  return Destination.aggregate([
-    {
+  const p = pipelineDestList();
+
+  if (country !== '') {
+    p.unshift({
       $match: {
         name: { $regex: nameRegexp },
-        country: country || '',
+        country: country,
       },
-    },
-    {
-      $lookup: {
-        from: 'likedestinations',
-        localField: '_id',
-        foreignField: '_destinationId',
-        as: 'likes',
+    });
+  } else {
+    p.unshift({
+      $match: {
+        name: { $regex: nameRegexp },
       },
-    },
-    {
-      $lookup: {
-        from: 'comments',
-        localField: '_id',
-        foreignField: '_destinationId',
-        as: 'comments',
-      },
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: '_ownerId',
-        foreignField: '_id',
-        as: 'owner',
-      },
-    },
-    {
-      $unwind: '$owner',
-    },
-    {
-      $project: {
-        name: 1,
-        country: 1,
-        description: 1,
-        img: '$img.thumbUrl',
-        _ownerId: 1,
-        likeCount: { $size: '$likes' },
-        commentCount: { $size: '$comments' },
-        ownerInfo: {
-          thumbUrl: '$owner.image.thumbUrl',
-          gender: '$owner.gender',
-          accountName: '$owner.accountName',
-        }
-      },
-    }
-  ]);
+    });
+  }
+
+  return Destination.aggregate(p);
 }
 
-async function getByUserId(userId, name, country, offset, pageSize) {
-  const nameRegexp = new RegExp(name, 'i');
+async function getByUserId(userId, offset, pageSize) {
+
   const _userId = new Types.ObjectId(userId);
-  if (country) {
-    return Destination.aggregate([
-      {
-        $match: {
-          _ownerId: _userId,
-          name: { $regex: nameRegexp },
-          country: country,
-        },
-      },
-      {
-        $lookup: {
-          from: 'likes',
-          localField: '_id',
-          foreignField: '_destinationId', // Assuming you have a field named 'destinationId' in the 'likes' collection
-          as: 'likes',
-        },
-      },
-      {
-        $lookup: {
-          from: 'comments',
-          localField: '_id',
-          foreignField: '_destinationId', // Assuming you have a field named 'destinationId' in the 'comments' collection
-          as: 'comments',
-        },
-      },
-      {
-        $project: {
-          name: 1,
-          country: 1,
-          description: 1,
-          img: '$img.thumbUrl',
-          _ownerId: 1,
-          likeCount: { $size: '$likes' },
-          commentCount: { $size: '$comments' },
-        },
-      },
-    ]);
-  }
-  return Destination.aggregate([
-    {
-      $match: {
-        _ownerId: _userId,
-        name: { $regex: nameRegexp },
-      },
+  const p = pipelineDestList();
+
+  p.unshift({
+    $match: {
+      _ownerId: _userId,
     },
-    {
-      $lookup: {
-        from: 'likes',
-        localField: '_id',
-        foreignField: '_destinationId', // Assuming you have a field named 'destinationId' in the 'likes' collection
-        as: 'likes',
-      },
-    },
-    {
-      $lookup: {
-        from: 'comments',
-        localField: '_id',
-        foreignField: '_destinationId', // Assuming you have a field named 'destinationId' in the 'comments' collection
-        as: 'comments',
-      },
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: '_ownerId',
-        foreignField: '_id',
-        as: 'owner',
-      },
-    },
-    {
-      $unwind: '$owner',
-    },
-    {
-      $project: {
-        name: 1,
-        country: 1,
-        description: 1,
-        img: '$img.thumbUrl',
-        _ownerId: 1,
-        likeCount: { $size: '$likes' },
-        commentCount: { $size: '$comments' },
-        ownerInfo: {
-          thumbUrl: '$owner.image.thumbUrl',
-          gender: '$owner.gender',
-          accountName: '$owner.accountName',
-        }
-      },
-    },
-  ]);
+  });
+
+  return Destination.aggregate(p);
 }
 
 async function getById(id) {
